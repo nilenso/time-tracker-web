4c2b78bc133fb3fdda5aaf2fd9ec23d85390e2ff
Abhik Khanra <abhik@nilenso.com>
2017-07-28 13:14:46 +0530

    Removing reducers code for InvoicePage as we no longer make an API call

--- src/containers/InvoicePage.js
+++ src/containers/InvoicePage.js
@@ -18,25 +18,17 @@ class InvoicePage extends Component {
   render() {
     const savedInvoice = this.props.entities
       .get('invoices')
-      .get(parseInt(this.props.params.invoiceId, 10));
+      .get(parseInt(this.props.params.invoiceId, 10)); //Base 10
 
-    if (this.props.isFetching || !savedInvoice) {
-      return (
-        <p>{"Invoice is being fetched, please wait."}</p>
-      )
-    }
-    else {
-      return (
-        <InvoiceTable invoice={savedInvoice} onSubmit={this.onInvoiceTableSubmit}/>
-      );
-    }
+    return (
+      <InvoiceTable invoice={savedInvoice} onSubmit={this.onInvoiceTableSubmit}/>
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
-    entities: state.get('entities'),
-    isFetching: state.get('invoicePage').get('isFetching')
+    entities: state.get('entities')
   };
 }
 
